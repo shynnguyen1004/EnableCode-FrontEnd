df2c66416264ec7990fe1ee43540e2f34ec2c615
phkhoiit-1508 <phkhoiit@gmail.com>
2026-05-31 17:28:07 +0700

    Revert "feat: setup axios client and auth api (#3)"
    This reverts commit 556e96da4f8b7a542d2701b27a88cb7315515841.

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { I18nProvider } from "./i18n/I18nProvider";
-import "./styles.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Root element not found");
-}
-
-createRoot(rootElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <I18nProvider>
-        <App />
-      </I18nProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
